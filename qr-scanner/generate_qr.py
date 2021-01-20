ce158bf9c350d0a445bb3b169f1d7f667de541f9
Loo Tung Lun <joshlootunglun@gmail.com>
2021-01-20 14:45:58 +0800

    integrate full end to end to system

--- qr-scanner/generate_qr.py
+++ qr-scanner/generate_qr.py
@@ -1,4 +1,7 @@
 import qrcode
 
-code = qrcode.make('Loo Tung Lun;010101-02-0303')
-code.save('tunglun_qr.png')
+#code = qrcode.make('Loo Tung Lun;010101-02-0303')
+#code.save('tunglun_qr.png')
+
+code = qrcode.make('Catherine Fletcher;121212-88-5656')
+code.save('catherine_qr.png')
--- system-reference/system.py
+++ system-reference/system.py
@@ -15,8 +15,25 @@ import adafruit_mlx90614
 import smbus
 import time
 import cv2
-import requests 
+import requests
+from gpiozero import Buzzer
+from time import sleep
+import RPi.GPIO as GPIO
 
+# RGB LED setup
+pinRed = 22
+pinGreen = 27
+pinBlue = 10
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(pinRed, GPIO.OUT)
+GPIO.setup(pinGreen, GPIO.OUT)
+GPIO.setup(pinBlue, GPIO.OUT)
+
+# buzzer setup
+buzzer = Buzzer(17)
 
 # defining the api-endpoint  
 API_ENDPOINT = "http://127.0.0.1:5000/api/users"
@@ -121,6 +138,10 @@ def main():
         # First, sample QR code, if there is QR code containing valid user info
         # then proceed to next step of temperature measurement
 
+        #idle mode
+        GPIO.output(pinRed, 1)
+        GPIO.output(pinGreen, 1)
+        GPIO.output(pinBlue, 1)
         lcd_string("",  LCD_LINE_1)
         lcd_string("Welcome, please ",  LCD_LINE_2)
         lcd_string("scan the QR code",  LCD_LINE_3)
@@ -128,7 +149,7 @@ def main():
 
         # QR code detection object
         detector = cv2.QRCodeDetector()
-
+        print(qr_done)
         if qr_done == 0:
             print(".")
             print("data: ", data, ". previous data: ", previousdata)
@@ -139,58 +160,96 @@ def main():
                 data, bbox, _ = detector.detectAndDecode(img)
                 
                 # if there is a bounding box, draw one, along with the data
-                """if(bbox is not None):
+                if(bbox is not None):
                     for i in range(len(bbox)):
                         cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
                                  0, 255), thickness=2)
                     cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                                0.5, (0, 255, 0), 2)"""
+                                0.5, (0, 255, 0), 2)
                 if data:
                     qr_done = 1
                     print("data found: ", data, "qr_done: ", qr_done)
                     previousdata = data
                     break
                 # display the image preview
-                #cv2.imshow("code detector", img)
-                #if(cv2.waitKey(1) == ord("q")):
-                #    break
+                cv2.imshow("code detector", img)
+                if(cv2.waitKey(1) == ord("q")):
+                    break
 
         # Received valid QR code, sense temperature, and then clear data.
         else:
+            #engaged mode
+            GPIO.output(pinRed, 0)
+            GPIO.output(pinGreen, 0)
+            GPIO.output(pinBlue, 1)
             print(",")
+            BodyTemp = round(mlx.object_temperature, 2)
+            print(BodyTemp)
             # This is the lowest human body temperature possible.
             # you are nearly dead if below 34.
-            while (round(mlx.object_temperature, 2) < 34):
-                temp_string = str(round(mlx.object_temperature, 2))
+            while (BodyTemp < 34):
+                temp_string = str(BodyTemp)
                 # Print temperature text
                 lcd_string("Wait for a valid",  LCD_LINE_1)
                 lcd_string("body temperature",  LCD_LINE_2)
                 lcd_string("                ",  LCD_LINE_3)
                 lcd_string("Current: " + temp_string + "C",LCD_LINE_4)
-                print(temp_string)
+                if (BodyTemp >34):
+                    break
+                BodyTemp = round(mlx.object_temperature, 2)
+                sleep(1)
+                print(BodyTemp)
+
+            if (BodyTemp < 38):
+                print("normal case.." + str(BodyTemp))
+                # normal body temperature mode
+                GPIO.output(pinRed, 0)
+                GPIO.output(pinGreen, 1)
+                GPIO.output(pinBlue, 0)
+                username = data.split(';')[0]
+                userid = data.split(';')[1]
+                temp_string = str(BodyTemp)
+                lcd_string("hi ",                   LCD_LINE_1)
+                lcd_string(username,                LCD_LINE_2)
+                lcd_string("Body temperature:",     LCD_LINE_3)
+                lcd_string(temp_string + " Celsius",LCD_LINE_4)
                 time.sleep(3)
 
-            username = data.split(';')[0]
-            userid = data.split(';')[1]
-            body_temp = round(mlx.object_temperature, 2)
-            temp_string = str(body_temp)
-            lcd_string("hi ",                   LCD_LINE_1)
-            lcd_string(username,                LCD_LINE_2)
-            lcd_string("Body temperature:",     LCD_LINE_3)
-            lcd_string(temp_string + " Celsius",LCD_LINE_4)
-            time.sleep(3)
+            else:
+                print("fever case.." + str(BodyTemp))
+                # high body temperature mode
+                GPIO.output(pinRed, 1)
+                GPIO.output(pinGreen, 0)
+                GPIO.output(pinBlue, 0)
+                username = data.split(';')[0]
+                userid = data.split(';')[1]
+                temp_string = str(BodyTemp)
+                lcd_string("FEVER!!! ",             LCD_LINE_1)
+                lcd_string(username,                LCD_LINE_2)
+                lcd_string("Body temperature:",     LCD_LINE_3)
+                lcd_string(temp_string + " Celsius",LCD_LINE_4)
+                buzzer.on()
+                time.sleep(3)
+                buzzer.off()
 
+
+                
             # data to be sent to api 
             data = {'username':     username,
                     'IC':           userid,
-                    'temperature':  body_temp} 
+                    'temperature':  BodyTemp} 
               
-            # sending post request and saving response as response object 
-            r = requests.post(url = API_ENDPOINT, json = data) 
-              
-            # extracting response text  
-            pastebin_url = r.text 
-            print("The pastebin URL is:%s"%pastebin_url) 
+            # sending post request and saving response as response object
+            try:
+                r = requests.post(url = API_ENDPOINT, json = data)
+                # extracting response text  
+                pastebin_url = r.text 
+                print("The pastebin URL is:%s"%pastebin_url) 
+            except:
+                print("Error occurred")
+            finally:
+                print("Finish sending to server")
+
             #clear previous data
             qr_done = 0
             data = ''
